0ffacf3633c15e3de994a6e91420daec3df9bfee
Byak-ko <maxim993511@gmail.com>
2025-03-06 19:38:09 +0000

    feat(acces): added role acces

---
+++ server/src/auth/roles.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLE_KEY } from './roles.decorator';
+import { Request } from 'express';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLE_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true; 
+    }
+
+    const request = context.switchToHttp().getRequest<Request>();
+    const user = (request as any).user;
+
+    if (!user || !requiredRoles.includes(user.role)) {
+      throw new ForbiddenException('Недостатньо прав');
+    }
+
+    return true;
+  }
+}
