2cfacf45de446e72ee2ab72e8ac889a5b3d4a66b
Byak-ko <maxim993511@gmail.com>
2025-03-06 19:36:47 +0000

    feat(db): added postgresql

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Department } from './entities/department.entity';
+import { Unit } from './entities/unit.entity';
+import { Rating } from './entities/rating.entity';
+import { RatingItem } from './entities/rating-item.entity';
+import { RatingApproval } from './entities/rating-approval.entity';
+import { Document } from './entities/document.entity';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { RatingsModule } from './ratings/rating.module';
+import { RatingItemsModule } from './rating-items/rating-items.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '11111111',
+      database: 'qualification',
+      entities: [User, Department, Unit, Rating, RatingItem, RatingApproval, Document], //what to chose
+      autoLoadEntities: true,
+      synchronize: true, //production off?
+    }),
+    TypeOrmModule.forFeature([User, Department, Unit, Rating, RatingItem, RatingApproval, Document]),
+    UsersModule,
+    AuthModule,
+    RatingsModule,
+    RatingItemsModule,
+  ],
 })
 export class AppModule {}
