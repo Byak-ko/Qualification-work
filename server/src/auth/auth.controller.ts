99c4827674001d8a3c2d55fe09641562cff559b5
Byak-ko <maxim993511@gmail.com>
2025-03-14 15:00:23 +0000

    feat: added refhresh and logout

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Req, Request, Body, Res, UseGuards} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('login')
   async login(@Body() body: { email: string; password: string }) {
-    return this.authService.validateUser(body.email, body.password).then(user => this.authService.login(user));
+    const { email, password } = body;
+    const user = await this.authService.validateUser(email, password);
+    return this.authService.login(user);
+  }
+
+  @Post('refresh')
+  async refresh(@Body() body: { refreshToken: string }) {
+    return this.authService.refreshToken(body.refreshToken);
+  }
+
+
+  @UseGuards(JwtAuthGuard)
+  @Post('logout')
+  async logout(@Request() req, @Res() res: Response) {
+    if (!req.user) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    await this.authService.logout(req.user.userId);
+    res.clearCookie('jwt');
+
+    return res.status(200).json({ message: 'Logged out successfully' });
   }
 }
