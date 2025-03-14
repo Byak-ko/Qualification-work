286f8f6e3cd232d9001f802b4e865f8a797d8b7b
Byak-ko <maxim993511@gmail.com>
2025-03-14 14:25:40 +0000

    feat: toast + theme added

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import AuthProvider from './components/AuthProvider.tsx'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router.tsx'
+import { ThemeProvider } from "./components/ThemeProvider"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
-
+<>
+  {/* ... */}
+  <ToastContainer position="top-right" autoClose={3000} />
+</>
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-   <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+      <ThemeProvider>
+         <AuthProvider>
+          <RouterProvider router={router} />
+          <ToastContainer position="top-right" autoClose={4000} theme="colored" />
+         </AuthProvider>
+      </ThemeProvider>
   </StrictMode>,
 )
