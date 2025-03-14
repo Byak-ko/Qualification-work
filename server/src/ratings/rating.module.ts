fb109e11947d31668ca9ac550dac07e4093567c5
Byak-ko <maxim993511@gmail.com>
2025-03-14 23:29:56 +0000

    update rating.module.ts

--- server/src/ratings/rating.module.ts
+++ server/src/ratings/rating.module.ts
@@ -1,9 +1,24 @@
-import { Module } from '@nestjs/common';
-import { RatingService } from './rating.service';
-import { RatingController } from './rating.controller';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { RatingService } from './rating.service'
+import { RatingController } from './rating.controller'
+
+import { Rating } from '../entities/rating.entity'
+import { User } from '../entities/user.entity'
+import { RatingItem } from '../entities/rating-item.entity'
+import { RatingApproval } from '../entities/rating-approval.entity'
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Rating,
+      User,
+      RatingItem,
+      RatingApproval,
+    ]),
+  ],
   providers: [RatingService],
-  controllers: [RatingController]
+  controllers: [RatingController],
+  exports: [RatingService],
 })
 export class RatingsModule {}
