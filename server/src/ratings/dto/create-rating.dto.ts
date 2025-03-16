2c9bb3c196ff824b29b8a3907eaa080a72632995
Byak-ko <maxim993511@gmail.com>
2025-03-16 19:22:13 +0000

    feat: create create-rating.dto.ts

---
+++ server/src/ratings/dto/create-rating.dto.ts
@@ -0,0 +1,33 @@
+import { IsArray, IsNotEmpty, IsString, ValidateNested, ArrayMinSize, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateRatingDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  type: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  respondentId: number;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  reviewerIds: number[];
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => RatingItemDto)
+  items: RatingItemDto[];
+}
+
+export class RatingItemDto {
+  @IsString()
+  name: string;
+
+  @IsNumber()
+  maxScore: number;
+}
