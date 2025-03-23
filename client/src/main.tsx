f4d67c552b26d7e223df41b7445bf850a3755019
Byak-ko <maxim993511@gmail.com>
2025-03-23 10:17:47 +0000

    update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,6 @@ import './index.css'
 import AuthProvider from './components/AuthProvider.tsx'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router.tsx'
-import { ThemeProvider } from "./components/ThemeProvider"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 
@@ -15,11 +14,9 @@ import "react-toastify/dist/ReactToastify.css"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <ThemeProvider>
          <AuthProvider>
           <RouterProvider router={router} />
           <ToastContainer position="top-right" autoClose={4000} theme="colored" />
          </AuthProvider>
-      </ThemeProvider>
   </StrictMode>,
 )
