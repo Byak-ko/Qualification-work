28419b91480af6ba62143522d720ee258b5086f1
Byak-ko <maxim993511@gmail.com>
2025-03-23 10:28:43 +0000

    update api.ts

--- client/src/services/api/api.ts
+++ client/src/services/api/api.ts
@@ -4,11 +4,3 @@ export const api = axios.create({
   baseURL: "http://localhost:3000",
   withCredentials: true, 
 });
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
