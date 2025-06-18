577dfa10d99c0e0fd923595fade37e4e7bc8f30e
Byak-ko <maxim993511@gmail.com>
2025-06-18 09:33:09 +0000

    refactor: remove unused test files and related service/controller code

--- server/src/rating-items/rating-items.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('rating-items')
-export class RatingItemsController {}
--- server/src/rating-items/rating-items.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RatingItemsService } from './rating-items.service';
-import { RatingItemsController } from './rating-items.controller';
-
-@Module({
-  providers: [RatingItemsService],
-  controllers: [RatingItemsController]
-})
-export class RatingItemsModule {}
